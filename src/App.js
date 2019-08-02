224d6b89545a14bd97e1125e7a4dca0eb2fb6fb0
rushikesh.mahindrakar@neosofttech.com <rushikesh.mahindrakar@neosofttech.com>
2019-08-02 20:04:47 +0530

    temporary ui added

--- src/App.js
+++ src/App.js
@@ -1,32 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { withAuthenticator } from 'aws-amplify-react'
-import Amplify, { Auth } from 'aws-amplify';
-import aws_exports from './aws-exports';
-Amplify.configure(aws_exports);
+import amplifyCustomUi from "aws-amplify-react-custom-ui";
+import SignIn from './signIn';
+import SecureApp from './secureApp';
 
 class App extends Component {
+
+  componentWillMount() {
+    amplifyCustomUi.setSignIn(SignIn);
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       <SecureApp />
       </div>
     );
   }
 }
 
-export default withAuthenticator(App, true);
+export default  App;
+
--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Amplify, { Auth } from 'aws-amplify';
+import * as aws_amplify_react from "aws-amplify-react";
+import amplifyCustomUi from "aws-amplify-react-custom-ui";
+//TODO : PLEASE PROVIDE THE aws-exports file :)
+import awsconfig from "./aws-exports";
+Amplify.configure(awsconfig);
+amplifyCustomUi.configure(aws_amplify_react);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
