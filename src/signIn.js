578f9c847b0d86db9bc29e1873d14a59b9d378de
rushikesh.mahindrakar@neosofttech.com <rushikesh.mahindrakar@neosofttech.com>
2019-08-12 15:39:39 +0530

    added new UI

--- src/signIn.js
+++ src/signIn.js
@@ -1,32 +1,41 @@
 import React, { Component } from "react";
 import { Auth } from "aws-amplify";
-const styles = {
-  continer: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  input: {
-    width: "100%",
-    padding: "12px 20px",
-    margin: "8px 0",
-    display: "inline-block",
-    border: "1px solid #ccc",
-    borderRadius: "4px",
-    boxSizing: "border-box"
-  },
-  submit: {
-    width: "100%",
-    backgroundColor: "#4CAF50",
-    color: "white",
-    padding: "14px 20px",
-    margin: "8px 0",
-    border: "none",
-    borderRadius: "4px",
-    cursor: "pointer"
-  }
-};
+
+
+import './loginfiles/bootstrap.min.css'
+import './loginfiles/style.css'
+import './loginfiles/responsive.css'
+
+
+
+
+// const styles = {
+//   continer: {
+//     display: "flex",
+//     flexDirection: "column",
+//     justifyContent: "center",
+//     alignItems: "center"
+//   },
+//   input: {
+//     width: "100%",
+//     padding: "12px 20px",
+//     margin: "8px 0",
+//     display: "inline-block",
+//     border: "1px solid #ccc",
+//     borderRadius: "4px",
+//     boxSizing: "border-box"
+//   },
+//   submit: {
+//     width: "100%",
+//     backgroundColor: "#4CAF50",
+//     color: "white",
+//     padding: "14px 20px",
+//     margin: "8px 0",
+//     border: "none",
+//     borderRadius: "4px",
+//     cursor: "pointer"
+//   }
+// };
 
 const updateByPropertyName = (propertyName, value) => () => ({
   [propertyName]: value
@@ -55,7 +64,9 @@ class SignIn extends Component {
 
     Auth.signIn(email, password)
       .then(user => {
-        this.setState(() => ({ ...INITIAL_STATE }));
+        console.log(user);
+        
+        //this.setState(() => ({ ...INITIAL_STATE }));
         if (
           user.challengeName === "SMS_MFA" ||
           user.challengeName === "SOFTWARE_TOKEN_MFA"
@@ -66,7 +77,9 @@ class SignIn extends Component {
         } else if (user.challengeName === "MFA_SETUP") {
           this.changeState("TOTPSetup", user);
         } else {
-          this.changeState("signedIn", user);
+         console.log(user);
+         localStorage.clear();
+
         }
       })
       .catch(err => {
@@ -90,47 +103,64 @@ class SignIn extends Component {
     const isInvalid = password === "" || email === "";
 
     return (
-      <div>
-        <div style={styles.continer}>
-          <h1>SignIn</h1>
-          <form onSubmit={this.onSubmit}>
-            <input
-              style={styles.input}
-              value={email}
-              onChange={event =>
-                this.setState(updateByPropertyName("email", event.target.value))
-              }
-              type="text"
-              placeholder="Email Address s"
-            />
-            <input
-              style={styles.input}
-              value={password}
-              onChange={event =>
-                this.setState(
-                  updateByPropertyName("password", event.target.value)
-                )
-              }
-              type="password"
-              placeholder="Password"
-            />
-            <button style={styles.submit} disabled={isInvalid} type="submit">
-              Sign In
-            </button>
-
-            {error && <p>{error.message}</p>}
-          </form>
-          <div>
-            <p> No account? </p>
-            <button
-              style={styles.submit}
-              onClick={() => this.changeState("signUp")}
-            >
-              Create account
-            </button>
-          </div>
-        </div>
+      <>
+      <div className="wrapper">
+	<nav className="navbar navbar-default">
+    <div className="container-fluid">
+      <div className="navbar-header">
+        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <span className="sr-only">Toggle navigation</span>
+          <span className="icon-bar"></span>
+          <span className="icon-bar"></span>
+          <span className="icon-bar"></span>
+        </button>        
       </div>
+      <div id="navbar" className="navbar-collapse collapse"> 
+      	<span className="nav-title">Language</span>       
+        <ul className="nav navbar-nav navbar-right">
+          <li><a href="#">English</a></li>
+          <li><a href="#">Spanish</a></li>
+          <li><a href="#">French</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <div className="container main-form-container">
+  	<div className="row">
+  		<div className="col-md-12">
+  			<div className="logo-container">
+          <a href="#">
+            <img src="https://staticcontent.inelcan.com/img/4gflota_logo.png" alt="4GFlota" className="img-responsive" />
+            </a>
+            </div>
+  		</div>
+  	</div>
+  	<div className="row">
+  		<div className="col-md-12">
+  			<form>
+  				<ul className="form-container">
+	  				<li>
+              <input type="text" name="" placeholder="User"/>
+              </li>
+	  				<li>
+              <input type="password" name="" placeholder="Password"/>
+              </li>
+	  				<li>
+              <button className="btn btn-default">Log In</button>
+              </li>
+	  			</ul>
+  			</form>
+  		</div>
+  	</div>
+  </div>
+
+  <footer>
+  	<p><a href="http://www.4gflota.com" target="_blank">www.4gflota.com</a></p>
+  </footer>
+
+</div>
+</>
     );
   }
 }
